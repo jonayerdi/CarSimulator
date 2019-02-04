9a13722aba751addaa4ed0b6acdc8287e7edd1bb
Jon Ayerdi <jon_ayerdi_33@hotmail.es>
2019-02-04 13:38:03 +0100

    handle inner rotation cases

--- js/Car.js
+++ js/Car.js
@@ -9,7 +9,7 @@ class Position {
         return Math.atan2(diffY, diffX);
     }
     rotateAround(center, perimeter, direction) {
-        // direction 1 = clockwise; -1 = counterclockwise
+        // direction: 1 = clockwise; -1 = counterclockwise
         let diffX = this.x - center.x;
         let diffY = this.y - center.y;
         let radius = Math.sqrt(diffX*diffX + diffY*diffY);
@@ -49,11 +49,13 @@ class Car {
         this.center.y = (this.left.y + this.right.y) / 2;
     }
     updateAngleFromWheels() {
-        this.right.angleRelative(this.left);
+        this.angle = this.right.angleRelative(this.left);
     }
     nextStep(timedelta) {
         let leftPerimeter = this.vleft * timedelta;
         let rightPerimeter = this.vright * timedelta;
+        let leftPerimeterAbs = Math.abs(leftPerimeter);
+        let rightPerimeterAbs = Math.abs(rightPerimeter);
         // No rotation
         if (leftPerimeter == rightPerimeter) {
             this.center.x += leftPerimeter * Math.sin(this.angle);
@@ -64,13 +66,29 @@ class Car {
         // With positive speed, left wheel rotates clockwise, right wheel rotates counterclockwise
         let leftDirection = this.vleft == 0 ? 0 : (this.vleft > 0 ? 1 : -1)
         let rightDirection = this.vright == 0 ? 0 : (this.vright > 0 ? -1 : 1)
-        // Wheels rotate in same of different directions
+        // Calculate rotation center
+        let rotationCenter = undefined;
+        // Wheels can rotate in same or different directions
         if (leftDirection == rightDirection) {
             // Both wheels clockwise or conterclockwise, rotation center between the wheels
-
+            let rotationCenterX = (this.left.x * rightPerimeterAbs + this.right.x * leftPerimeterAbs) / (leftPerimeterAbs + rightPerimeterAbs);
+            let rotationCenterY = (this.left.y * rightPerimeterAbs + this.right.y * leftPerimeterAbs) / (leftPerimeterAbs + rightPerimeterAbs);
+            rotationCenter = new Position(rotationCenterX, rotationCenterY);
         } else {
             // Both wheels moving forwards or backwards, rotation center left or right of the car
+            // Special cases, only one wheel moving
+            if (leftPerimeter == 0) {
+                rotationCenter = this.left;
+            } else if(rightPerimeter == 0) {
+                rotationCenter = this.right;
+            } else {
+                // Rotation center somewhere left or right of the car
 
+            }
         }
+        this.left.rotateAround(rotationCenter, leftPerimeterAbs, leftDirection);
+        this.right.rotateAround(rotationCenter, rightPerimeterAbs, rightDirection);
+        this.updateCenterFromWheels();
+        this.updateAngleFromWheels();
     }
 }
